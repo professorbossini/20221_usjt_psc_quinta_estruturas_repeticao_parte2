952cc64728e8bd73f6a019628fffb98535b82dd2
Rodrigo Bossini <professorbossini@gmail.com>
2022-03-31 21:31:21 -0300

    Agora fazendo com for

--- Exercicio1.java
+++ Exercicio1.java
@@ -9,6 +9,28 @@ public class Exercicio1{
         n = leitor.nextInt();
         //repetição controlada por contador
         
+        for (Integer contador = 1; contador <= n; contador = contador + 1){
+            //lançar o primeiro dado
+            int d1 = gerador.nextInt(6) + 1;
+            //int d1 = gerador.nextInt(7);
+            int d2 = gerador.nextInt(6) + 1;
+            //4 6: 4 < 6
+            //1 1: 1 = 1
+            //2 1: 2 > 1
+            // if (d1 < d2)
+            //     System.out.printf ("%d %c %d\n", d1, '<', d2);
+            // else if (d1 == d2)
+            //     System.out.printf("%d = %d\n", d1, d2);
+            // else
+            //     System.out.println (d1 + " > " + d2);
+            //teste ? v1 : v2
+            System.out.printf(
+                "%d %c %d\n",
+                d1,
+                d1 < d2 ? '<' : d1 == d2 ? '=' : '>',
+                d2
+            ); 
+        }
         Integer contador = 1;
         while(contador <= n){//condição de continuidade
             //lançar o primeiro dado
