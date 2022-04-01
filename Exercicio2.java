af8ced38c65fc621ad11c965a09647b1a1beec91
Rodrigo Bossini <professorbossini@gmail.com>
2022-03-31 21:35:46 -0300

    Incluindo o modelo para o exercicio 2

---
+++ Exercicio2.java
@@ -0,0 +1,11 @@
+public class Exercicio2 {
+    public static void main(String[] args) {
+        int numero = ..;
+        //1 sortear um valor inteiro entre 1 e 10
+
+        
+        //2 fazer um loop (for ou while) que permite que o usuário tente adivinhar qual foi o número sorteado
+        //3 quando o usuário adivinhar, o programa dá parabéns para ele e termina
+        //Dica: operador para comparar por diferença é o !=
+    }
+}
