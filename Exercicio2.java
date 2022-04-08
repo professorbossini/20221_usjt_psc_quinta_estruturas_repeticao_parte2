96d6bb57e5409f9d2432c24d32d9665122b395bd
Rodrigo Bossini <professorbossini@gmail.com>
2022-04-07 21:26:51 -0300

    Desafio de adivinhacao de numero com contador

--- Exercicio2.java
+++ Exercicio2.java
@@ -1,11 +1,32 @@
+import java.util.Random;
+import javax.swing.JOptionPane;
+
 public class Exercicio2 {
     public static void main(String[] args) {
-        int numero = ..;
+        int numeroOculto;
+        int palpiteDoUsuario;
+        int contador;
+        Random gerador = new Random();
         //1 sortear um valor inteiro entre 1 e 10
-
-        
+        numeroOculto = gerador.nextInt(10) + 1;
+        //System.out.println(numeroOculto);
+        //JOptionPane.showMessageDialog(null, numeroOculto);
         //2 fazer um loop (for ou while) que permite que o usuário tente adivinhar qual foi o número sorteado
+        palpiteDoUsuario = Integer.parseInt(JOptionPane.showInputDialog("Qual o seu palpite"));
+        contador = 1;
+
+        while(palpiteDoUsuario != numeroOculto){
+            palpiteDoUsuario = Integer.parseInt(JOptionPane.showInputDialog("Escolha outro"));
+            //contador = contador + 1;
+            //contador += 1;
+            //contador++;
+            ++contador;
+
+        }
         //3 quando o usuário adivinhar, o programa dá parabéns para ele e termina
+        JOptionPane.showMessageDialog(null, "Parabéns, o número oculto era mesmo o " + numeroOculto + ". Você tentou " + contador + " vezes.");
+
+
         //Dica: operador para comparar por diferença é o !=
     }
 }
